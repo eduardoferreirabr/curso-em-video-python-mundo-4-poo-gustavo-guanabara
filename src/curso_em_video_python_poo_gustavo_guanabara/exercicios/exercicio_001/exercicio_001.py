c235187d97ebe1fdbc70b52a8ad96c94a7a52a15
eduardoferreirabr <eduardociviluff@gmail.com>
2026-09-03 12:52:38 -0300

    feat(exercicios): adciona exercicio-001

---
+++ src/curso_em_video_python_poo_gustavo_guanabara/exercicios/exercicio_001/exercicio_001.py
@@ -0,0 +1,32 @@
+# Declaração de classe
+
+
+class Gafanhoto:
+    def __init__(self):  # Método Construtor
+        # Atributos de Instância
+        self.nome = ""
+        self.idade = 0
+
+    # Métodos de instância
+    def aniversario(self):
+        self.idade += 1
+
+    def mensagem(self):
+        return f"{self.nome} é Gafanhoto(a) e tem {self.idade} anos de idade."
+
+
+# Declaração de Objetos
+
+g1 = Gafanhoto()
+g1.nome = "Maria"
+g1.idade = 17
+g1.aniversario()
+print(g1.mensagem())
+
+g2 = Gafanhoto()
+g2.nome = "Mauro"
+g2.idade = 53
+print(g2.mensagem())
+
+g3 = Gafanhoto()
+print(g3.mensagem())
