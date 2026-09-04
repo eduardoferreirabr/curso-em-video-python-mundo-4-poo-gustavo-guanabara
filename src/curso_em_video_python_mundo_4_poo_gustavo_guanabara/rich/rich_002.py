decde47d677f6bdb3fd6c8958ae654b91d8f30b7
eduardoferreirabr <eduardociviluff@gmail.com>
2026-09-04 14:56:15 -0300

    feat(rich): adiciona rich 001, 002, 003, 004 e 005

---
+++ src/curso_em_video_python_mundo_4_poo_gustavo_guanabara/rich/rich_002.py
@@ -0,0 +1,11 @@
+from rich import print
+from rich.panel import Panel
+
+caixa = Panel(
+    "[white]Esse aqui é um painel de exemplo[/]",
+    title="Mensagem",
+    style="red",
+    width=35,
+)
+
+print(caixa)
