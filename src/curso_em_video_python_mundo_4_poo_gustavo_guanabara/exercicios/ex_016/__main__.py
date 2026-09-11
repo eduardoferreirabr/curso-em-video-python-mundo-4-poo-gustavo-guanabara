b1b6d0127c2b5daa1c55fcc7474c6001c34e9935
eduardoferreirabr <eduardociviluff@gmail.com>
2026-09-10 22:47:53 -0300

    feat(exercicios): adiciona ex-016

---
+++ src/curso_em_video_python_mundo_4_poo_gustavo_guanabara/exercicios/ex_016/__main__.py
@@ -0,0 +1,17 @@
+from classes import *
+
+
+def main():
+    a = Porta()
+    b = Empresa()
+    c = Ovo()
+    d = Pedra()
+
+    tentar_abrir(a)
+    tentar_abrir(b)
+    tentar_abrir(c)
+    tentar_abrir(d)  # Vai dar erro, pois a classe Pedra não tem o método abrir()
+
+
+if __name__ == "__main__":
+    main()
